59a0eae0b2750b0d809e2efaa0a10779a48f304a
Marek Eibel <536376@mail.muni.cz>
2025-09-07 12:57:46 +0200

    feat: rename temporary page ServerStatusPage

--- src/ui/router/routes.tsx
+++ src/ui/router/routes.tsx
@@ -2,14 +2,14 @@ import { Navigate, type RouteObject } from "react-router-dom";
 import Home from "../pages/Home.tsx";
 import Settings from "../pages/Settings.tsx";
 import { Counter } from "../pages/Counter.tsx";
-import ServerStatus from "../pages/ServerStatus.tsx";
+import ServerStatusPage from "../pages/ServerStatusPage.tsx";
 
 const routes: RouteObject[] = [
     { path: "/", element: <Navigate to="/home" /> },
     { path: "/home", Component: Home },
     { path: "/settings", Component: Settings },
     { path: "/counter", Component: Counter },
-    { path: "/server-status", Component: ServerStatus },
+    { path: "/server-status", Component: ServerStatusPage },
     { path: "*", element: <Navigate to="/" /> },
 ];
 
