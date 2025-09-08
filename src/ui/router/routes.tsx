ddb6bece86dc180ce7189f0247cb335ca8ebab56
Marek Eibel <536376@mail.muni.cz>
2025-09-08 08:33:48 +0200

    feat: all pages now will use MainLayout componet

--- src/ui/router/routes.tsx
+++ src/ui/router/routes.tsx
@@ -2,15 +2,26 @@ import { Navigate, type RouteObject } from "react-router-dom";
 import Home from "../pages/Home.tsx";
 import Settings from "../pages/Settings.tsx";
 import { Counter } from "../pages/Counter.tsx";
-import ServerStatusPage from "../pages/ServerStatusPage.tsx";
+import MainLayout from "../layouts/MainLayout.tsx";
 
 const routes: RouteObject[] = [
-    { path: "/", element: <Navigate to="/home" /> },
-    { path: "/home", Component: Home },
-    { path: "/settings", Component: Settings },
-    { path: "/counter", Component: Counter },
-    { path: "/server-status", Component: ServerStatusPage },
-    { path: "*", element: <Navigate to="/" /> },
+    {
+        path: "/",
+        element: <Navigate to="/home" />,
+    },
+    {
+        path: "/",
+        Component: MainLayout,
+        children: [
+            { path: "home", Component: Home },
+            { path: "settings", Component: Settings },
+            { path: "counter", Component: Counter },
+        ],
+    },
+    {
+        path: "*",
+        element: <Navigate to="/" />,
+    },
 ];
 
 export default routes;
