4bedb2746242edde4966b2cf43cbad854b6c881e
Marek Eibel <536376@mail.muni.cz>
2025-09-08 12:33:31 +0200

    feat: update MainLayout with Header

--- src/ui/layouts/MainLayout.tsx
+++ src/ui/layouts/MainLayout.tsx
@@ -2,10 +2,14 @@ import { Outlet } from "react-router-dom";
 
 import "./MainLayout.css";
 import { Footer } from "../components/footer/Footer";
+import { Header } from "../components/header/Header";
 
 export function MainLayout() {
     return (
         <div className={"layout"}>
+            <div className="layout__header">
+                <Header />
+            </div>
             <div className="layout__content">
                 <Outlet />
             </div>
