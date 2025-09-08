a353772ffa218202984fb37b0f59ecdd0c9edd7d
Marek Eibel <536376@mail.muni.cz>
2025-09-08 08:32:06 +0200

    feat: add translations for ServerStatus component

--- src/ui/components/server-status/ServerStatus.tsx
+++ src/ui/components/server-status/ServerStatus.tsx
@@ -1,35 +1,53 @@
 import { useServerStatus } from "../../hooks/useServerStatus";
+import { useTranslation } from "react-i18next";
 
 import "./ServerStatus.css";
 
 export function ServerStatus() {
     const { isLoading, error } = useServerStatus();
+    const { t } = useTranslation();
 
     if (isLoading) {
         return (
-            <div className="serverStatus">
-                Server status:{" "}
-                <span title="Connecting..." className="dot dot--yellow"></span>
+            <div
+                title={t("global.ServerStatus.Connecting...")}
+                className="serverStatus"
+            >
+                <span className="serverStatus__label">
+                    {t("global.ServerStatus.Server status")}:
+                </span>
+                <span className="dot dot--yellow"></span>
             </div>
         );
     }
 
     if (error) {
         return (
-            <div className="serverStatus">
-                Server status:{" "}
-                <span
-                    title={`Server has ended with an error: <${error.message}>! Go to Settings for more information.`}
-                    className="dot dot--red"
-                ></span>
+            <div
+                title={`${t(
+                    "global.ServerStatus.Server has ended with an error"
+                )}: <${error.message}>! ${t(
+                    "global.ServerStatus.Go to Settings for more information."
+                )}`}
+                className="serverStatus"
+            >
+                <span className="serverStatus__label">
+                    {t("global.ServerStatus.Server status")}:
+                </span>
+                <span className="dot dot--red"></span>
             </div>
         );
     }
 
     return (
-        <div className="serverStatus">
-            Server status:{" "}
-            <span title="Server is running." className="dot dot--green"></span>
+        <div
+            title={t("global.ServerStatus.Server is running.")}
+            className="serverStatus"
+        >
+            <span className="serverStatus__label">
+                {t("global.ServerStatus.Server status")}:
+            </span>
+            <span className="dot dot--green"></span>
         </div>
     );
 }
