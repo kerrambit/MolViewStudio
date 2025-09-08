f3dab7224a499efe36e7705abf2c373656829356
Marek Eibel <536376@mail.muni.cz>
2025-09-08 08:32:56 +0200

    feat: add MainLayout with a Footer compoment

---
+++ src/ui/layouts/MainLayout.tsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+
+import "./MainLayout.css";
+import { Footer } from "../components/footer/Footer";
+
+export function MainLayout() {
+    return (
+        <div className={"layout"}>
+            <div className="layout__content">
+                <Outlet />
+            </div>
+            <div className="layout__footer">
+                <Footer />
+            </div>
+        </div>
+    );
+}
+
+export default MainLayout;
