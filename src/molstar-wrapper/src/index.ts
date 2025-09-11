68a3510565c42a935403129639ee9eb10020cc4e
Marek Eibel <536376@mail.muni.cz>
2025-09-11 21:35:35 +0200

    feat: implement molstar wrapper with its own package.json and tsconfig.json

---
+++ src/molstar-wrapper/src/index.ts
@@ -0,0 +1,128 @@
+import { createPluginUI } from "molstar/lib/mol-plugin-ui";
+import { renderReact18 } from "molstar/lib/mol-plugin-ui/react18";
+import { PluginUIContext } from "molstar/lib/mol-plugin-ui/context";
+import { DefaultPluginUISpec } from "molstar/lib/mol-plugin-ui/spec";
+// import { Asset } from "molstar/lib/mol-util/assets";
+
+interface MolstarProps {
+    showControls: boolean;
+    isExpanded: boolean;
+}
+
+let molstar: PluginUIContext | undefined;
+
+export async function initMolstar(
+    container: HTMLDivElement,
+    props: MolstarProps
+) {
+    if (molstar) return molstar;
+
+    molstar = await createPluginUI({
+        target: container,
+        render: renderReact18,
+        spec: {
+            layout: {
+                initial: {
+                    regionState: {
+                        bottom: "hidden",
+                        left: "hidden",
+                        right: "hidden",
+                        top: "hidden",
+                    },
+                    showControls: props.showControls,
+                    isExpanded: props.isExpanded,
+                },
+            },
+            behaviors: [...DefaultPluginUISpec().behaviors],
+        },
+    });
+
+    // molstar.behaviors.interaction.click.subscribe(
+    //     ({ current, button, modifiers }) => {
+    //         if (!current.loci) return;
+
+    //         if (button === 2) {
+    //             console.log("Right click!");
+    //         }
+
+    //         console.log("Clicked!", { current, button, modifiers });
+
+    //         if (StructureElement.Loci.is(current.loci)) {
+    //             const location = StructureElement.Loci.getFirstLocation(
+    //                 current.loci
+    //             );
+    //             if (location) {
+    //                 const element = location.unit.model.atomicHierarchy.atoms;
+    //                 const name = element.type_symbol.value(0);
+    //                 console.log(`Clicked on: ${name}.`);
+    //             }
+    //         }
+    //     }
+    // );
+
+    // molstar.behaviors.interaction.hover.subscribe(({ current }) => {
+    //     console.log("Hovered:", current);
+    // });
+
+    return molstar;
+}
+
+export function disposeMolstar() {
+    molstar?.dispose();
+    molstar = undefined;
+}
+
+export async function clearViewer() {
+    if (!molstar) throw new Error("Molstar is not initialized!");
+    await molstar.clear();
+}
+
+export async function loadDefaultPbdStructure() {
+    if (!molstar) throw new Error("Molstar is not initialized!");
+
+    await clearViewer();
+
+    const data = await molstar.builders.data.download(
+        { url: "https://files.rcsb.org/download/3PTB.pdb" },
+        { state: { isGhost: true } }
+    );
+
+    const trajectory = await molstar.builders.structure.parseTrajectory(
+        data,
+        "pdb"
+    );
+    const preset = await molstar.builders.structure.hierarchy.applyPreset(
+        trajectory,
+        "default"
+    );
+
+    return preset;
+}
+
+// export async function loadStructureFromFile() {
+//     if (!molstar) throw new Error("Molstar is not initialized!");
+
+//     await molstar.clear();
+
+//     const fileData = await window.electron.openFileExplorer();
+//     if (!fileData) return;
+
+//     const file = new File([fileData.content], fileData.name);
+//     const assetFile = Asset.File(file);
+
+//     const fileResult = await molstar.builders.data.readFile({
+//         file: assetFile,
+//         isBinary: fileData.binary,
+//     });
+
+//     const trajectory = await molstar.builders.structure.parseTrajectory(
+//         fileResult.data,
+//         fileData.extension as any
+//     );
+//     const preset = await molstar.builders.structure.hierarchy.applyPreset(
+//         trajectory,
+//         "default"
+//     );
+
+//     return preset;
+// }
