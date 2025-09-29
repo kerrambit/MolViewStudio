cd9c9db4306971c3c833b8409740c54dee7945a3
Marek Eibel <536376@mail.muni.cz>
2025-09-29 20:16:01 +0200

    feat: change mantine dragzone to the react dropzone so custom validator could be implemented

--- src/ui/components/common/dropzone/Dropzone.tsx
+++ src/ui/components/common/dropzone/Dropzone.tsx
@@ -1,67 +1,105 @@
-import {
-    Dropzone as MantineDropZone,
-    type FileRejection,
-    type FileWithPath,
-} from "@mantine/dropzone";
-import {
-    IconDragDrop,
-    IconUpload,
-    IconX,
-    type ReactNode,
-} from "@tabler/icons-react";
+import { type FileRejection } from "@mantine/dropzone";
+import { useDropzone } from "react-dropzone";
+import { IconDragDrop, type ReactNode } from "@tabler/icons-react";
 
 import "./Dropzone.css";
 
-export type Extension = string;
-
-export type Accept = {
-    [mimeType: string]: Extension[];
-};
-
 export interface DropzoneProps {
-    onDrop: (files: FileWithPath[]) => void;
+    onDrop: (files: File[]) => void;
     onReject?: ((fileRejections: FileRejection[]) => void) | undefined;
     enableMultipleInputFiles?: boolean;
     children?: ReactNode;
-    accept?: Accept;
+    allowedExtensions?: string[];
 }
 
 export function Dropzone(props: DropzoneProps) {
+    const { /*acceptedFiles, fileRejections,*/ getRootProps, getInputProps } =
+        useDropzone({
+            validator: createValidator(props.allowedExtensions ?? []),
+            noClick: true,
+            multiple: props.enableMultipleInputFiles,
+            onDropAccepted(files, _) {
+                props.onDrop(files);
+            },
+            onDropRejected(fileRejections, _) {
+                if (props.onReject) {
+                    props.onReject(fileRejections);
+                }
+            },
+        });
+
+    // const acceptedFileItems = acceptedFiles.map((file) => (
+    //     <li key={file.path}>
+    //         {file.path} - {file.size} bytes
+    //     </li>
+    // ));
+
+    // const fileRejectionItems = fileRejections.map(({ file, errors }) => (
+    //     <li key={file.path}>
+    //         {file.path} - {file.size} bytes
+    //         <ul>
+    //             {errors.map((e) => (
+    //                 <li key={e.code}>{e.message}</li>
+    //             ))}
+    //         </ul>
+    //     </li>
+    // ));
+
     return (
-        <MantineDropZone
-            className="dropzone"
-            onDrop={props.onDrop}
-            onReject={props.onReject}
-            activateOnClick={false}
-            multiple={props.enableMultipleInputFiles}
-            accept={props.accept}
-        >
-            <div className="dropzone__body">
-                {props.children}
-                <div className="dropzone__state">
-                    <MantineDropZone.Accept>
-                        <IconUpload
-                            size={160}
-                            color="var(--mantine-color-blue-6)"
-                            stroke={1.5}
-                        />
-                    </MantineDropZone.Accept>
-                    <MantineDropZone.Reject>
-                        <IconX
-                            size={160}
-                            color="var(--mantine-color-red-6)"
-                            stroke={1.5}
-                        />
-                    </MantineDropZone.Reject>
-                    <MantineDropZone.Idle>
+        <section className="container dropzone">
+            <div {...getRootProps({ className: "dropzone" })}>
+                <input {...getInputProps()} />
+                <div className="dropzone__body">
+                    {props.children}
+                    <div className="dropzone__state">
                         <IconDragDrop
                             size={160}
                             color="var(--mantine-color-dimmed)"
                             stroke={1.5}
                         />
-                    </MantineDropZone.Idle>
+                    </div>
                 </div>
             </div>
-        </MantineDropZone>
+        </section>
     );
+
+    // return (
+    //     <MantineDropZone
+    //         className="dropzone"
+    //         onDrop={props.onDrop}
+    //         onReject={props.onReject}
+    //         activateOnClick={false}
+    //         multiple={props.enableMultipleInputFiles}
+    //     >
+    //         <div className="dropzone__body">
+    //             {props.children}
+    //             <div className="dropzone__state">
+    //                 <MantineDropZone.Idle>
+    //                     <IconDragDrop
+    //                         size={160}
+    //                         color="var(--mantine-color-dimmed)"
+    //                         stroke={1.5}
+    //                     />
+    //                 </MantineDropZone.Idle>
+    //             </div>
+    //         </div>
+    //     </MantineDropZone>
+    // );
+}
+
+function createValidator(allowedExtensions: string[]) {
+    return (file: File) => {
+        const extension = file.name.split(".").pop()?.toLowerCase();
+        if (
+            !extension ||
+            !allowedExtensions.map((e) => e.toLowerCase()).includes(extension)
+        ) {
+            return {
+                code: "unsupported-file-extension",
+                message: `Unsupported extension <${file.name}>!`,
+            };
+        }
+
+        return null;
+    };
 }
