848032a965506f988b70b9e7d275759df6e563b7
Marek Eibel <536376@mail.muni.cz>
2025-09-30 20:01:47 +0200

    feat: add TopBar component which generalize header
    header now uses topbar as a container component

--- src/ui/components/header/Header.tsx
+++ src/ui/components/header/Header.tsx
@@ -1,17 +1,18 @@
 import { Avatar } from "./avatar/Avatar";
 import { Menu } from "./menu/Menu";
 import { NotificationBell } from "./notification-bell/NotificationBell";
+import { TopBar } from "../common/topbar/TopBar";
 
 import "./Header.css";
 
 export function Header() {
     return (
-        <header className="header">
+        <TopBar>
             <Menu></Menu>
             <div className="header__right-side">
                 <NotificationBell></NotificationBell>
                 <Avatar></Avatar>
             </div>
-        </header>
+        </TopBar>
     );
 }
