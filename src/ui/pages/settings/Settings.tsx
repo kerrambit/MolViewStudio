50b5d04f6e5e90fe08760515b37f17b9ba534e4d
Marek Eibel <536376@mail.muni.cz>
2025-10-06 20:43:24 +0200

    feat: implement settings navigation tree

--- src/ui/pages/settings/Settings.tsx
+++ src/ui/pages/settings/Settings.tsx
@@ -1,18 +1,94 @@
-import { useTranslation } from "react-i18next";
+import {
+    useLocation,
+    useNavigate,
+    type NavigateFunction,
+} from "react-router-dom";
 import { IconSettingsFilled } from "@tabler/icons-react";
+import Watermark from "../../components/common/watermark/Watermark";
+
+import "./Settings.css";
 
 export default function Settings() {
-    const { t } = useTranslation();
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const pathParts = location.pathname
+        .split("/")
+        .filter(Boolean)
+        .map((part) => part.charAt(0).toUpperCase() + part.slice(1));
 
     return (
-        <div>
-            <h1>{t("Settings")}</h1>
-
-            <IconSettingsFilled
-                size={160}
-                color="var(--mantine-color-dimmed)"
-                stroke={1.5}
-            />
+        <div className="settings">
+            {/* <h1>{pathParts.join(" - ")}</h1> */}
+            <h1>Settings</h1>
+            <Watermark icon={IconSettingsFilled} />
+            {renderTree(navigate)}
         </div>
+    );
+}
+
+function renderTreeLink(
+    navigate: NavigateFunction,
+    name: string,
+    path: string
+) {
+    return (
+        <li
+            onClick={() => {
+                navigate("/settings/" + path);
+            }}
+            className="settings__tree__link"
+        >
+            {name}
+        </li>
+    );
+}
+
+function renderTree(navigate: NavigateFunction) {
+    return (
+        <ul className="settings__tree">
+            <li>
+                General
+                <ul>
+                    {renderTreeLink(navigate, "Language", "general/language")}
+                    <br></br>
+                    {renderTreeLink(navigate, "UI", "general/ui")}
+                    <br></br>
+                    {renderTreeLink(
+                        navigate,
+                        "Notifications",
+                        "general/notifications"
+                    )}
+                    <br></br>
+                    {renderTreeLink(navigate, "Help", "general/help")}
+                </ul>
+            </li>
+
+            {renderTreeLink(navigate, "Account", "account")}
+            <br></br>
+            {renderTreeLink(navigate, "Server", "server")}
+
+            <li>
+                Formats
+                <ul>
+                    {renderTreeLink(navigate, "Input", "formats/input")}
+                    <br></br>
+                    {renderTreeLink(navigate, "Export", "formats/export")}
+                </ul>
+            </li>
+
+            <li>
+                Processing
+                <ul>
+                    {renderTreeLink(navigate, "General", "processing/general")}
+                    <br></br>
+                    {renderTreeLink(
+                        navigate,
+                        "Per Format",
+                        "processing/per-format"
+                    )}
+                </ul>
+            </li>
+        </ul>
     );
 }
--- src/ui/router/routes.tsx
+++ src/ui/router/routes.tsx
@@ -21,10 +21,54 @@ const routes: RouteObject[] = [
             { path: "sidebar", Component: SidebarPage },
         ],
     },
+
     {
         path: "/settings",
         Component: BackNavigationLayout,
-        children: [{ index: true, Component: Settings }],
+        children: [
+            { index: true, Component: Settings },
+            {
+                path: "general",
+                Component: Settings,
+                children: [
+                    { path: "language", Component: Settings },
+                    { path: "ui", Component: Settings },
+                    { path: "notifications", Component: Settings },
+                    { path: "help", Component: Settings },
+                ],
+            },
+            {
+                path: "account",
+                Component: Settings,
+                children: [{ index: true, Component: Settings }],
+            },
+            {
+                path: "server",
+                Component: Settings,
+                children: [{ index: true, Component: Settings }],
+            },
+            {
+                path: "account",
+                Component: Settings,
+                children: [{ index: true, Component: Settings }],
+            },
+            {
+                path: "formats",
+                Component: Settings,
+                children: [
+                    { path: "input", Component: Settings },
+                    { path: "export", Component: Settings },
+                ],
+            },
+            {
+                path: "processing",
+                Component: Settings,
+                children: [
+                    { path: "general", Component: Settings },
+                    { path: "per-format", Component: Settings },
+                ],
+            },
+        ],
     },
     {
         path: "/old-settings",
