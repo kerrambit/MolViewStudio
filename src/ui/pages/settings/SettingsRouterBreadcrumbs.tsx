b27d1b04e41f9104902dd7b2fc26c23850267c5e
Marek Eibel <536376@mail.muni.cz>
2025-10-06 21:53:44 +0200

    feat: implement setting breadcrumbs

---
+++ src/ui/pages/settings/SettingsRouterBreadcrumbs.tsx
@@ -0,0 +1,28 @@
+import { RouterBreadcrumbs } from "../../components/common/router-readcrumbs/RouterBreadcrumbs";
+
+interface SettingsRouterBreadcrumbsProps {
+    autoSavedPage: boolean;
+}
+
+export function SettingsRouterBreadcrumbs(
+    props: SettingsRouterBreadcrumbsProps
+) {
+    const wordsToFix = {
+        words: [{ word: "ui", fixed: "UI" }],
+    };
+    return (
+        <div>
+            <RouterBreadcrumbs wordsToFix={wordsToFix} />
+            {props.autoSavedPage && (
+                <div
+                    title="Auto-save means that when you make any change in the page, the change is automatically registered and set."
+                    style={{
+                        fontSize: "0.9em",
+                    }}
+                >
+                    This page is auto-saved.
+                </div>
+            )}
+        </div>
+    );
+}
