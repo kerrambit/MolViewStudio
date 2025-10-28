bf12e36e5ee24c5ea4e95a8384639b5fe3138707
Marek Eibel <536376@mail.muni.cz>
2025-10-28 15:34:17 +0100

    feat: implement new view card creator

---
+++ src/ui/components/view-card/NewViewCardCreator.tsx
@@ -0,0 +1,123 @@
+import { useState } from "react";
+import {
+    getCanvasImageAsUri,
+    useLiveCameraState,
+    type Base64Png,
+} from "../../../molstar-wrapper/src";
+import { UnstyledTextInput } from "../common/input/UnstyledTextInput";
+import { Button } from "../common/button/Button";
+import { Vec3 } from "molstar/lib/mol-math/linear-algebra/3d";
+import { TextInput } from "@mantine/core";
+
+import "./NewViewCardCreator.css";
+
+export interface NewViewCardCreatorProps {
+    index: number;
+    onSave?: (
+        position: Vec3 | undefined,
+        up: Vec3 | undefined,
+        target: Vec3 | undefined,
+        title: string,
+        id: string,
+        thumbnail: Base64Png | undefined
+    ) => void;
+}
+
+export function NewViewCardCreator(props: NewViewCardCreatorProps) {
+    const [currentName, setCurrentName] = useState<string | undefined>(
+        "Enter name for new view..."
+    );
+
+    const cameraState = useLiveCameraState();
+
+    return (
+        <div className="newViewCardCreator">
+            <UnstyledTextInput
+                prefix={`${props.index}. `}
+                defaultValue="Enter name for new view..."
+                placeholder="Enter name for new view..."
+                tooltip="Enter name for new view..."
+                onValueChange={(val) => setCurrentName(val)}
+                style={{
+                    margin: "1em",
+                }}
+            />
+
+            <TextInput
+                label="Position"
+                value={
+                    cameraState?.position
+                        ? `${cameraState.position[0].toFixed(
+                              3
+                          )}, ${cameraState.position[1].toFixed(
+                              3
+                          )}, ${cameraState.position[2].toFixed(3)}`
+                        : "Not defined"
+                }
+                readOnly
+            />
+
+            <TextInput
+                label="Up"
+                value={
+                    cameraState?.up
+                        ? `${cameraState.up[0].toFixed(
+                              3
+                          )}, ${cameraState.up[1].toFixed(
+                              3
+                          )}, ${cameraState.up[2].toFixed(3)}`
+                        : "Not defined"
+                }
+                readOnly
+            />
+
+            <TextInput
+                label="Target"
+                value={
+                    cameraState?.target
+                        ? `${cameraState.target[0].toFixed(
+                              3
+                          )}, ${cameraState.target[1].toFixed(
+                              3
+                          )}, ${cameraState.target[2].toFixed(3)}`
+                        : "Not defined"
+                }
+                readOnly
+            />
+
+            <div
+                style={{
+                    display: "flex",
+                    paddingBottom: "1em",
+                    justifyContent: "center",
+                }}
+            >
+                <Button
+                    size="small"
+                    tooltip="This view will be saved."
+                    label="Save view"
+                    variant="secondary"
+                    onClick={async () => {
+                        let img: Base64Png | undefined;
+                        try {
+                            img = await getCanvasImageAsUri();
+                        } catch {
+                            img = undefined;
+                        }
+
+                        if (props.onSave && currentName && cameraState) {
+                            props.onSave(
+                                cameraState.position,
+                                cameraState.up,
+                                cameraState.target,
+                                currentName,
+                                crypto.randomUUID(),
+                                img
+                            );
+                        }
+                    }}
+                ></Button>
+            </div>
+        </div>
+    );
+}
