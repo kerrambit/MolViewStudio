7e54c9e71da21f40104531e0e0320d3c66756c4f
Marek Eibel <536376@mail.muni.cz>
2025-10-28 15:34:02 +0100

    feat: implement ViewCard

---
+++ src/ui/components/view-card/ViewCard.tsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { Button } from "../common/button/Button";
+import { UnstyledTextInput } from "../common/input/UnstyledTextInput";
+
+import "./ViewCard.css";
+
+export interface ViewCardProps {
+    id: string;
+    title: string;
+    index: number;
+    onClick?: () => void;
+    thumbnail?: string;
+    onSave?: (newTitle: string) => void;
+}
+
+export function ViewCard(props: ViewCardProps) {
+    const [_, setCurrentName] = useState<string | undefined>(props.title);
+
+    return (
+        <div key={`$view:${props.id}`} className="viewCard">
+            <div
+                style={{
+                    width: "100%",
+                }}
+            >
+                <UnstyledTextInput
+                    prefix={`${props.index + 1}. `}
+                    defaultValue={props.title}
+                    placeholder="Enter name for new view..."
+                    tooltip="Enter name for new view..."
+                    onValueChange={(val) => {
+                        setCurrentName(val);
+                        if (val) {
+                            props.onSave?.(val);
+                        }
+                    }}
+                    style={{
+                        margin: "1em",
+                    }}
+                />
+            </div>
+
+            <img
+                onClick={() => {
+                    if (props.onClick) {
+                        props.onClick();
+                    }
+                }}
+                style={{
+                    cursor: "pointer",
+                    maxWidth: "90%",
+                    borderRadius: "6px",
+                }}
+                title="Click to set the current camera position to this view."
+                key={`$img:${props.id}`}
+                src={props.thumbnail}
+                alt={`${props.title} - thumbnail`}
+            />
+            <div
+                style={{
+                    display: "flex",
+                    paddingTop: "1em",
+                    paddingBottom: "1em",
+                }}
+            >
+                <Button
+                    size="small"
+                    tooltip="The current camera position will be set to this view."
+                    label="Load view"
+                    variant="secondary"
+                    onClick={() => {
+                        if (props.onClick) {
+                            props.onClick();
+                        }
+                    }}
+                ></Button>
+            </div>
+        </div>
+    );
+}
