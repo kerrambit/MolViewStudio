00f56c672434586f67f4a40684845d2478fc54a1
Marek Eibel <536376@mail.muni.cz>
2026-02-15 18:22:22 +0100

    feat: import optimization

--- src/server/server.py
+++ src/server/server.py
@@ -1,13 +1,13 @@
 from typing import List
-from result import Ok, Err, Result, is_ok
+from result import Ok, Err, Result, is_ok  # type: ignore
 from pathlib import Path
-from fastapi import FastAPI, Response, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI, Response, HTTPException  # type: ignore
+from pydantic import BaseModel  # type: ignore
 import shutil
-from volsegtools.converter import MapConverter
-from volsegtools.preprocessor import PreprocessorBuilder, Preprocessor
-from volsegtools.downsampler import HierarchyDownsampler
-from volsegtools.model.working_store import WorkingStore
+from volsegtools.converter import MapConverter  # type: ignore
+from volsegtools.preprocessor import PreprocessorBuilder, Preprocessor  # type: ignore
+from volsegtools.downsampler import HierarchyDownsampler  # type: ignore
+from volsegtools.model.working_store import WorkingStore  # type: ignore
 
 
 def _process_volume(filepath: str, temporary_directory: str) -> Result[List[str], str]:
