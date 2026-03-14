4c331c6785abb4e2fc684c5421cd10281b39223f
Marek Eibel <536376@mail.muni.cz>
2026-03-14 09:50:45 +0100

    feat: remove unsused Molstar provider context
    snapshot is saved as part of restoring regime

--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -4,7 +4,6 @@ import routes from "./router/routes.tsx";
 import { UserSettingsProvider } from "./services/UserSettingsProvider.tsx";
 import { ThemeProvider } from "./services/ThemeProvider.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { MolstarProvider } from "./services/MolstarProvider.tsx";
 import { FileDataProvider } from "./services/FileDataProvider.tsx";
 import { MenuProvider } from "./services/MenuProvider.tsx";
 
@@ -21,14 +20,12 @@ createRoot(document.getElementById("root")!).render(
         <ThemeProvider>
             <UserSettingsProvider>
                 <FileDataProvider>
-                    <MolstarProvider>
-                        <MenuProvider
-                            isDev={env.isDev}
-                            navigate={(path) => router.navigate(path)}
-                        >
-                            <RouterProvider router={router} />
-                        </MenuProvider>
-                    </MolstarProvider>
+                    <MenuProvider
+                        isDev={env.isDev}
+                        navigate={(path) => router.navigate(path)}
+                    >
+                        <RouterProvider router={router} />
+                    </MenuProvider>
                 </FileDataProvider>
             </UserSettingsProvider>
         </ThemeProvider>
