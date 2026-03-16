06f82b03ff0b706e99e33774849efcac9f3c815c
Marek Eibel <536376@mail.muni.cz>
2026-03-16 20:58:14 +0100

    feat: add notifications support

--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -6,8 +6,10 @@ import { AppearanceProvider } from "./services/AppearanceProvider.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RegimeProvider } from "./services/RegimeProvider.tsx";
 import { MenuProvider } from "./services/MenuProvider.tsx";
+import { Notifications } from "@mantine/notifications";
 
 import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
 import "./index.css";
 
 const router = createMemoryRouter(routes);
@@ -19,14 +21,16 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
         <UserSettingsProvider>
             <AppearanceProvider>
-                <RegimeProvider>
-                    <MenuProvider
-                        isDev={env.isDev}
-                        navigate={(path) => router.navigate(path)}
-                    >
-                        <RouterProvider router={router} />
-                    </MenuProvider>
-                </RegimeProvider>
+                <Notifications>
+                    <RegimeProvider>
+                        <MenuProvider
+                            isDev={env.isDev}
+                            navigate={(path) => router.navigate(path)}
+                        >
+                            <RouterProvider router={router} />
+                        </MenuProvider>
+                    </RegimeProvider>
+                </Notifications>
             </AppearanceProvider>
         </UserSettingsProvider>
     </QueryClientProvider>,
