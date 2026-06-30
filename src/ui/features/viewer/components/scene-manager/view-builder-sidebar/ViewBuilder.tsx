3fbfd604ba8a9fae12908816a427184c925cb66e
Marek Eibel <536376@mail.muni.cz>
2026-06-30 11:59:57 +0200

    fix: scroll to fiest selected asset or if there is expanded asset, then to the expanded

--- src/ui/features/viewer/components/scene-manager/view-builder-sidebar/ViewBuilder.tsx
+++ src/ui/features/viewer/components/scene-manager/view-builder-sidebar/ViewBuilder.tsx
@@ -41,9 +41,12 @@ export function ViewBuilder(props: ViewBuilderProps) {
     if (!view) return <></>;
 
     // Computes index of first selected asset in the list, otherwise -1.
-    const firstSelectedAssetIndex = assetsInView.findIndex((asset) =>
-        selectedAssetIds.includes(asset.id),
-    );
+    const firstSelectedAssetIndex =
+        expandedAssetId === null
+            ? assetsInView.findIndex((asset) =>
+                  selectedAssetIds.includes(asset.id),
+              )
+            : assetsInView.findIndex((asset) => asset.id === expandedAssetId);
 
     // Render the component.
     return (
